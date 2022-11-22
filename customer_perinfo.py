54ecb6b9df2af4531d770fc8376d082ad8a425f1
kboring1 <kboring1@ivytech.edu>
2022-11-22 14:11:59 -0500

    inital database

---
+++ customer_perinfo.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Table, Column, Integer,String, MetaData
+engine = create_engine('sqlite:///customerdata.db', echo = True)
+
+meta = MetaData()
+customers = Table(
+    'customers personal data', meta,
+    Column('id', Integer, primary_key = True),
+    Column('address', String,),
+    Column('Phone Number', Integer),
+    Column('Card Number', Integer) 
+)
+
+conn = engine.connect()
+conn.execute(customers.insert(),[
+    {'address': '', 'Phone Number' : '', 'Card Number' :''},
+    {'address': '', 'Phone Number' : '', 'Card Number' :''},
+    {'address': '', 'Phone Number' : '', 'Card Number' :''},
+    {'address': '', 'Phone Number' : '', 'Card Number' :''},
+    {'address': '', 'Phone Number' : '', 'Card Number' :''}
+])
+
+meta.create_all(engine)
